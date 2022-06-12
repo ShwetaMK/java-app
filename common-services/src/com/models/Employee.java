867ad05c38c57771a6dd77998d8cfa9e41bd7a03
ShwetaMK <shweta13mk@gmail.com>
2022-06-12 16:24:33 +0530

    test1

---
+++ common-services/src/com/models/Employee.java
@@ -0,0 +1,23 @@
+package com.models;
+
+public class Employee {
+
+	public String name;
+	public String phone;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
